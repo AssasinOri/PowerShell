cc25410689f6806a1aaa050ba5e33052ccf9e514
AssasinOri <art.yakushev@gmail.com>
2021-01-05 12:13:20 +0100

    Fixed problem with snake movement

--- snake.ps1
+++ snake.ps1
@@ -1,115 +1,126 @@
+function draw-borders ($x1,$y1,$x2,$y2) {
+    [char]9556+"$([char]9552)"*30+[char]9559+"`n"+[char]9553+"`n"+[char]9553+"`n"+[char]9562+"$([char]9552)"*30+[char]9565
+}
 function global:set-cursorposition ($x,$y) {
     [console]::CursorLeft = $x
     [console]::CursorTop = $y
 }
-
 function global:draw-symbol ($x,$y,$symbol) {
     set-cursorposition $x $y
     Write-Host $symbol -NoNewline
 }
-$windowWidth = [console]::WindowWidth - 1
-$WindowHeight = [console]::WindowHeight - 1
-[console]::CursorVisible = $false
-
 function draw-snake ($snake) {
     for ($i=0;$i -lt $snake.count;$i++) {
         draw-symbol $snake[$i][$x] $snake[$i][$y] "*" 
     }
 }
+function Set-ArrowArray ($ArrowArray) {
+    for ($i = $ArrowArray.Count-1; $i -gt 0; $i--) {
+        $ArrowArray[$i] = $ArrowArray[$i-1]
+    }    
+}
 
-$xArrow = 0
-$yArrow = 0
+function gameover {
+    set-cursorposition ([int][math]::round($windowWidth/2)-4) ([int][math]::round($WindowHeight/2))
+    [System.Console]::BackgroundColor = "red"
+    Write-Host "Game over" -NoNewline
+    sleep 5
+    exit
+}
 
+$windowWidth = [console]::WindowWidth - 1
+$WindowHeight = [console]::WindowHeight - 1
+[console]::CursorVisible = $false
+
+#Set coordinates of @ element
 $xx = Get-Random -Minimum 0 -Maximum $windowWidth
 $yy = Get-Random -Minimum 0 -Maximum $WindowHeight
 
+#Set coordinates of # element (bomb)
 $xbomb = Get-Random -Minimum 0 -Maximum $windowWidth
 $ybomb = Get-Random -Minimum 0 -Maximum $WindowHeight
 
+#Initialize the first element of snake array 
+$snake = @()
+$snake += ,(@([int][math]::round($windowWidth/2),[int][math]::round($WindowHeight/2)))
+#set indexes of x and y in snake array
 $x = 0
 $y = 1
-#$snake = @(@([math]::round($windowWidth/2),[math]::round($WindowHeight/2)))
-$snake = @(0)
-$snake[0] = @(@(50,10))
+
 Clear-Host
-$snakespeedchange = 10
+#set speed of snake
 $snakespeed = 150 #milliseconds
+
+#Initialize first direction
 $keypressed = "RightArrow" #right direction
-$OldArrow = $keypressed
+$ArrowArray = @()
+$ArrowArray += $keypressed
 while ($true) {
-    #draw snake
     draw-symbol $xx $yy "@"
     draw-symbol $xbomb $ybomb "#"
-    while (-not [Console]::KeyAvailable) {
-        Start-Sleep -Milliseconds $snakespeed
-        #erase last snake element
-        draw-snake $snake
-        draw-symbol 0 0 $snake
-        draw-symbol 0 1 $snake.count
-        switch ($keypressed) {
+    if ( [Console]::KeyAvailable) {
+        $keypressed = ([Console]::ReadKey($true)).key
+    }
+    if ($keypressed -eq "UpArrow" -or $keypressed -eq "DownArrow" -or $keypressed -eq "LeftArrow" -or $keypressed -eq "RightArrow") {
+        if ($ArrowArray.count -gt 1){
+            if ($ArrowArray[0] -eq "RightArrow" -and $keypressed -eq "LeftArrow"){gameover}
+            if ($ArrowArray[0] -eq "LeftArrow" -and $keypressed -eq "RightArrow"){gameover}
+            if ($ArrowArray[0] -eq "UpArrow" -and $keypressed -eq "DownArrow"){gameover}
+            if ($ArrowArray[0] -eq "DownArrow" -and $keypressed -eq "UpArrow"){gameover}
+        }
+        Set-ArrowArray $ArrowArray
+        $ArrowArray[0] = $keypressed
+    }
+    Start-Sleep -Milliseconds $snakespeed
+    #helping code
+    draw-symbol 0 0 $snake
+    draw-symbol 0 1 $snake.count
+    draw-symbol 0 2 $ArrowArray
+    draw-symbol 0 3 $keypressed
+    $lastSnakeElementX = $snake[$snake.Count-1][$x]
+    $lastSnakeElementY = $snake[$snake.Count-1][$y]
+    for ($i = 0; $i -lt $snake.Count; $i++) {
+        switch ($ArrowArray[$i]) {
             "UpArrow" {
-                for ($i=0;$i -lt $snake.count;$i++) {
-                    if ($snake[$i][$x] -eq $xArrow){
-                        if ($snake[$i][$y] -gt 0) {$snake[$i][$y]=$snake[$i][$y]-1}
-                    }
-                }
+                if ($snake[$i][$y] -gt 0) {$snake[$i][$y]=$snake[$i][$y]-1}
             }
             "DownArrow" {
-                for ($i=0;$i -lt $snake.count;$i++) {
-                    if ($snake[$i][$x] -eq $xArrow){
-                        if ($snake[$i][$y] -lt $WindowHeight) {$snake[$i][$y]=$snake[$i][$y]+1}
-                    }
-                }
+                if ($snake[$i][$y] -lt $WindowHeight) {$snake[$i][$y]=$snake[$i][$y]+1}
             }              
             "RightArrow" {
-                for ($i=0;$i -lt $snake.count;$i++) {
-                    if ($snake[$i][$y] -eq $yArrow){
-                        if ($snake[$i][$x] -lt $windowWidth) {$snake[$i][$x]=$snake[$i][$x]+1}
-                    }
-                }
+                if ($snake[$i][$x] -lt $windowWidth) {$snake[$i][$x]=$snake[$i][$x]+1}    
             }
             "LeftArrow" {
-                for ($i=0;$i -lt $snake.count;$i++) {
-                    if ($snake[$i][$y] -eq $yArrow){
-                        if ($snake[$i][$x] -gt 0) {$snake[$i][$x]=$snake[$i][$x]-1}
-                    }
-                }
+                if ($snake[$i][$x] -gt 0) {$snake[$i][$x]=$snake[$i][$x]-1}
             }      
         } #switch keypressed
-        draw-snake $snake
-#        draw-symbol $snake[$snake.Count-1][$x] $snake[$snake.Count-1][$y] " "
-        if ($snake[0][$y] -eq $yy -and $snake[0][$x] -eq $xx) {
-            if ($snakespeed -gt $snakespeedchange){
-                $snakespeed = $snakespeed - $snakespeedchange            
-                $snake += 0
-                switch ($keypressed) {
-                    "UpArrow"       {$snake[$snake.count-1] = @($xx,($yy+$snake.count-1))}
-                    "DownArrow"     {$snake[$snake.count-1] = @($xx,($yy-$snake.count-1))}
-                    "LeftArrow"     {$snake[$snake.count-1] = @(($xx+$snake.count-1),$yy)}
-                    "RightArrow"    {$snake[$snake.count-1] = @(($xx-$snake.count-1),$yy)}
-                }
-                $xx = Get-Random -Minimum 0 -Maximum $windowWidth
-                $yy = Get-Random -Minimum 0 -Maximum $WindowHeight
-                draw-symbol $xx $yy "@"
-            }
+        if ($i -gt 0) {
+            if (($snake[0][$x] -eq $snake[$i][$x])-and($snake[0][$y] -eq $snake[$i][$y])){gameover}
         }
-<#        if ($y -eq $ybomb -and $x -eq $xbomb) {
-            draw-symbol $xbomb $ybomb "!"
-#            set-cursorposition $WindowWidth/2-5 $WindowHeight/2
-#            [System.Console]::BackgroundColor = "red"
-            draw-symbol 0 0 "game over"
-            sleep 5
-            exit
+    } #for $snake
+    draw-snake $snake
+    draw-symbol $lastSnakeElementX $lastSnakeElementY " "
+    # If snake get into the @
+    if ($snake[0][$y] -eq $yy -and $snake[0][$x] -eq $xx) {
+        if ($snakespeed -gt [int][math]::round($snakespeed/10)){
+            $snakespeed = $snakespeed - [int][math]::round($snakespeed/10)
+        }           
+        $snake += ,(@($lastSnakeElementX,$lastSnakeElementY))
+        if ($keypressed -eq "UpArrow" -or $keypressed -eq "DownArrow" -or $keypressed -eq "LeftArrow" -or $keypressed -eq "RightArrow") {
+            $ArrowArray += $ArrowArray[$ArrowArray.count-1]
         }
-        if ($y -eq 0 -or $y -eq $WindowHeight -or $x -eq 0 -or $x -eq $windowWidth){  
-            draw-symbol 10 10 "game over"
-            sleep 5
-            exit
-        } #>
-    } #while keyAvailable
-    $OldArrow = $keypressed
-    $keypressed = ([Console]::ReadKey($true)).key
-    $xArrow = $snake[0][$x]
-    $yArrow = $snake[0][$y]
-    draw-symbol 0 20 $keypressed
+        $xx = Get-Random -Minimum 0 -Maximum $windowWidth
+        $yy = Get-Random -Minimum 0 -Maximum $WindowHeight
+        draw-symbol $xx $yy "@"
+    }
+    # If snake get into the Bomb #
+    if ($snake[0][$y] -eq $ybomb -and $snake[0][$x] -eq $xbomb) {
+        draw-symbol $xbomb $ybomb "!"
+        gameover
+    }
+<#    if ($y -eq 0 -or $y -eq $WindowHeight -or $x -eq 0 -or $x -eq $windowWidth){  
+        draw-symbol 10 10 "game over"
+        sleep 5
+        exit
+    } #>
 }
